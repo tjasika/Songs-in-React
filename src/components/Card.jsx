d76a034cef569a9a027cda2c951f45a2ffbd98c0
Tjaša <tjasazzeleznik@gmail.com>
2025-07-24 20:04:35 +0200

    Styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,11 @@ export const Card = ({title, artist, album, image}) => {
     return (
         <>
             <div className="bg-zinc-900 w-50 h-70 shadow-xl rounded-lg flex flex-col justify-center items-center">
-                <img src={image} className="w-45 h-45"></img>
-                <div className='w-45 text-white'>
-                    <h1 className='text-bold truncate'>{title}</h1>
-                    <h3 className='text-medium truncate'>{artist}</h3>
-                    <p className='text-bold truncate'>{album}</p>
+                <img src={image} className="w-45 h-45 rounded-sm"></img>
+                <div className='w-45 text-gray-200 pt-2'>
+                    <h1 className='font-bold text-lg truncate'>{title}</h1>
+                    <h3 className='text-sm truncate'>{artist}</h3>
+                    <p className='text-sm text-zinc-500 truncate'>{album}</p>
                 </div>
                 
             </div>
